7d11d0e1ac534e62943f89ff5263b47e5c458adb
Iam-VM <vishnumohanantheking@gmail.com>
2020-08-16 15:34:13 +0530

    netlify test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,72 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import "../styles/utils.css";
+import indexStyles from "../styles/index.module.css";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  return (
+    <div className={indexStyles.main}>
+      {/*cover*/}
+      <div className={indexStyles.coverContainer}></div>
 
-export default IndexPage
+      {/*ieee wie */}
+      <div className={indexStyles.container}>
+        <div className={indexStyles.headContainer}>
+          <h1 className={indexStyles.headDegree2}>IEEE WiE</h1>
+          <div className={indexStyles.underline}></div>
+        </div>
+        <div className={indexStyles.paraContainer}>
+          <p className={indexStyles.standardPara}>
+            IEEE Women in Engineering (WIE) is a global network of IEEE members and volunteers dedicated to promoting
+            women engineers and scientists, and inspiring girls around the world to follow their academic interests in a
+            career in engineering and science.
+          </p>
+          <p className={indexStyles.standardPara}>
+             IEEE is the world’s largest technical professional organization dedicated to advancing technology for the
+             benefit of humanity.
+          </p>
+        </div>
+       </div>
+
+       {/*mission and vision*/}
+      <div className={indexStyles.container}>
+        <div className={indexStyles.headContainer}>
+          <h1 className={indexStyles.headDegree2}>Mission & Vision</h1>
+          <div className={indexStyles.underline}></div>
+        </div>
+        <div className={indexStyles.paraContainer}>
+          <p className={indexStyles.standardPara}>
+            Our goal is to facilitate the recruitment and retention of women in technical disciplines globally. We
+            envision a vibrant community of IEEE women and men collectively using their diverse talents to innovate for
+            the benefit of humanity.
+          </p>
+        </div>
+      </div>
+
+       {/*wie ag - ks*/}
+       <div className={indexStyles.container}>
+         <div className={indexStyles.headContainer}>
+           <h1 className={indexStyles.headDegree2}>WiE AG - Kerala Section</h1>
+           <div className={indexStyles.underline}></div>
+         </div>
+         <div className={indexStyles.paraContainer}>
+           <p className={indexStyles.standardPara}>
+             Kerala Section WIE affinity group has always been an active and vibrant group with passionate volunteers.
+             Membership status and quality of programs have shown a tremendous hike in the last two years.
+           </p>
+           <p className={indexStyles.standardPara}>
+             Apart Apart from the usual chores of WIE Congress and face to face meetings, WIE Star and WIE Week Celebration are also key
+             highlights. Each year is showcased with new initiatives and an ocean of ideas bringing benefits and
+             development to the society .
+           </p>
+         </div>
+       </div>
+
+      {/*carousel*/}
+
+
+     </div>
+  );
+};
+
+export default IndexPage;
