2e0bc770c16d75306533ef2054129e4bd347a5de
Iam-VM <vishnumohanantheking@gmail.com>
2020-08-19 21:12:37 +0530

    modified carousel settings

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,23 +1,25 @@
-import React, {useState} from "react";
+import React, {useState, CSSProperties} from "react";
 import carouselStyles from "../styles/carouselStyles.module.css";
 import {FaAngleDoubleRight, FaAngleDoubleLeft} from "react-icons/fa";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 const CustomCarousel = () => {
+
+
   return (
-    <Carousel>
+    <Carousel infiniteLoop useKeyboardArrows autoPlay showThumbs={false} showStatus={false} interval={3000} transitionTime={500}>
       <div>
         <img src="wieTeam.png" alt={"WiE Team"}/>
-        <p className="legend">WiE Team</p>
+        {/*<p className="legend">WiE Team</p>*/}
       </div>
       <div>
         <img src="designTeam.png" alt={"Design Team"}/>
-        <p className="legend">Design Team</p>
+        {/*<p className="legend">Design Team</p>*/}
       </div>
       <div>
         <img src="contentTeam.png" alt={"Content Team"}/>
-        <p className="legend">Content Team</p>
+        {/*<p className="legend">Content Team</p>*/}
       </div>
     </Carousel>
   );
