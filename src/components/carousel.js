c2f9cca0586b2471c31a386727074dcb161c6942
Iam-VM <vishnumohanantheking@gmail.com>
2020-08-20 12:36:20 +0530

    changed button and carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,12 +1,12 @@
 import React, {useState, CSSProperties} from "react";
 import carouselStyles from "../styles/carouselStyles.module.css";
 import {FaAngleDoubleRight, FaAngleDoubleLeft} from "react-icons/fa";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "../styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
 const CustomCarousel = () => {
   return (
-    <Carousel className={carouselStyles.carousel} infiniteLoop useKeyboardArrows autoPlay showThumbs={false} showStatus={false} interval={3000} transitionTime={500} swipeScrollTolerance={0.5}>
+    <Carousel style={""}className={carouselStyles.carousel} infiniteLoop useKeyboardArrows autoPlay showThumbs={false} showStatus={false} interval={3000} transitionTime={500} swipeScrollTolerance={0.5}>
       <div className={carouselStyles.slide}>
         <img src="wieTeam.png" alt={"WiE Team"} className={carouselStyles.carouselImage}/>
         {/*<p className="legend">WiE Team</p>*/}
