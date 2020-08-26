2aec4d66405fd704c61a61a946fd444a4a0f4d96
Iam-VM <vishnumohanantheking@gmail.com>
2020-08-26 23:32:39 +0530

    prple rectangle size increased

---
+++ src/components/indexNew/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import footerStyles from "./footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={footerStyles.footerContainer}>
+
+    </div>
+  );
+}
+
+export default Footer;
