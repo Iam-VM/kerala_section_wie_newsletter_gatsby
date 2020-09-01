6a196848753799260cdde505a20c2cb4e9a3e579
Iam-VM <vishnumohanantheking@gmail.com>
2020-09-01 22:46:38 +0530

    with footer - modified

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
-import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import errorPageStyles from "../styles/errorPage.module.css";
+import Footer from "../components/indexNew/footer"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+  <div className={errorPageStyles.container}>
+    <Footer />
+  </div>
+);
 
 export default NotFoundPage
