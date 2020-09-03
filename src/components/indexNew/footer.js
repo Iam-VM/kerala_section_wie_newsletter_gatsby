ac8101b4ed381af370d53588f8962b5f50385ab5
Iam-VM <vishnumohanantheking@gmail.com>
2020-09-04 00:51:33 +0530

    with changed footer animation time aos

--- src/components/indexNew/footer.js
+++ src/components/indexNew/footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
   }, []);
 
   return (
-    <div data-aos-once="true" data-aos="zoom-in" data-aos-duration="800" className={footerStyles.footerContainer}>
+    <div data-aos-once="true" data-aos="zoom-in" data-aos-duration="400" className={footerStyles.footerContainer}>
       <div className={footerStyles.devNamesContainer}>
         <div className={footerStyles.devNamesHeadContainer}>
           <div className={footerStyles.devHead}>Dev</div>
